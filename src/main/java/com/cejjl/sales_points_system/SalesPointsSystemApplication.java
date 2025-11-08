71b1c90a8b6c64511381aa840214908c6ec000ab
Joao Vitor <diassenai56@gmail.com>
2025-11-08 15:45:46 -0300

    first commit

---
+++ src/main/java/com/cejjl/sales_points_system/SalesPointsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cejjl.sales_points_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesPointsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesPointsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cejjl/sales_points_system/SalesPointsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cejjl.sales_points_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesPointsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
