e6528186136cc9ab9858d5ba9a1eea71b1d300ab
lucasgls <lucasglsilva7@gmail.com>
2025-11-30 18:08:01 -0300

    feat(posto): implement posto module to funcionario CRUD

--- src/main/java/com/cejjl/sales_points_system/controllers/posto/PostoController.java
+++ src/main/java/com/cejjl/sales_points_system/controllers/posto/PostoController.java
@@ -1,4 +1,25 @@
 package com.cejjl.sales_points_system.controllers.posto;
 
+import com.cejjl.sales_points_system.models.posto.Posto;
+import com.cejjl.sales_points_system.services.posto.PostoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/postos")
 public class PostoController {
+
+    @Autowired
+    private PostoService postoService;
+
+    @PostMapping
+    public ResponseEntity<Posto> criar(@RequestBody Posto posto) {
+        Posto novoPosto = postoService.criar(posto);
+
+        return ResponseEntity.ok(novoPosto);
+    }
 }
