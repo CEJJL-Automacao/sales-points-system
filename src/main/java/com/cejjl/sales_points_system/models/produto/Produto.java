df0cc44dc19809b3ae8ebba14dfcc9b98c202404
Joao Vitor <diassenai56@gmail.com>
2025-12-06 09:52:50 -0300

    added: Adicionando entidades de Grupo e Produto

---
+++ src/main/java/com/cejjl/sales_points_system/models/produto/Produto.java
@@ -0,0 +1,29 @@
+package com.cejjl.sales_points_system.models.produto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    private Grupo grupo;
+
+}
