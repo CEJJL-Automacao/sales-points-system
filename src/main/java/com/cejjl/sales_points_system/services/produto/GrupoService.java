a7991818006ff680d1f2d721887ce3dc86b905af
lucasgls <lucasglsilva7@gmail.com>
2025-12-19 23:03:43 -0300

    feat(grupo): add request DTO

--- src/main/java/com/cejjl/sales_points_system/services/produto/GrupoService.java
+++ src/main/java/com/cejjl/sales_points_system/services/produto/GrupoService.java
@@ -1,7 +1,7 @@
 package com.cejjl.sales_points_system.services.produto;
 
+import com.cejjl.sales_points_system.dtos.request.GrupoRequest;
 import com.cejjl.sales_points_system.models.produto.Grupo;
-import com.cejjl.sales_points_system.models.produto.Produto;
 import com.cejjl.sales_points_system.repositories.produto.GrupoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -18,41 +18,42 @@ public class GrupoService {
     private final GrupoRepository grupoRepository;
 
     @Transactional
-    public Grupo adicionarGrupo(Grupo grupo){
+    public Grupo adicionarGrupo(GrupoRequest request) {
         try {
+            Grupo grupo = new Grupo();
+            grupo.setNome(request.nome());
+            grupo.setPontos(request.pontos());
             return grupoRepository.save(grupo);
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new RuntimeException("Erro ao tentar salvar grupo no banco.");
         }
     }
 
     @Transactional
-    public List<Grupo> buscarTodosGrupos(){
+    public List<Grupo> buscarTodosGrupos() {
         return grupoRepository.findAll();
     }
 
     @Transactional
-    public Grupo buscarGrupoPorId(UUID id){
+    public Grupo buscarGrupoPorId(UUID id) {
         return grupoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Grupo não encontrado"));
     }
 
     @Transactional
-    public Grupo atualizarGrupoPorId(UUID id, Grupo grupoAtualizado){
-
+    public Grupo atualizarGrupoPorId(UUID id, GrupoRequest request) {
         Grupo grupo = buscarGrupoPorId(id);
 
-        Grupo novoGrupo = Grupo.builder()
-                .id(grupo.getId())
-                .nome(grupoAtualizado.getNome() == null ? grupo.getNome() : grupoAtualizado.getNome())
-                .pontos(grupoAtualizado.getPontos() == null ? grupo.getPontos() : grupoAtualizado.getPontos())
-                .build();
+        if (request.nome() != null)
+            grupo.setNome(request.nome());
+        if (request.pontos() != null)
+            grupo.setPontos(request.pontos());
 
-        return grupoRepository.save(novoGrupo);
+        return grupoRepository.save(grupo);
     }
 
     @Transactional
-    public void deletarGrupoPorId(UUID id){
+    public void deletarGrupoPorId(UUID id) {
         if (grupoRepository.findById(id).isEmpty()) {
             throw new RuntimeException("Grupo não encontrado com o ID: " + id);
         }
